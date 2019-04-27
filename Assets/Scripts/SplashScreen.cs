29594e008876df919f4ec80a113b847bbf2aabf9
Lara <lara.durrant@gmail.com>
2019-04-27 09:09:29 -0700

    Initial Commit

---
+++ Assets/Scripts/SplashScreen.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SplashScreen : MonoBehaviour {
+
+	public float loadTime;
+	private float waitTilTime;
+	
+	public LevelManager lv;
+
+	void Start()
+	{
+		
+		waitTilTime = Time.time + loadTime;
+		
+	}
+
+	void Update()
+	{
+		
+		
+		if(Time.time > waitTilTime)
+		{
+			lv.LoadLevel("Start Menu");
+		}
+		
+	}
+	
+
+}
