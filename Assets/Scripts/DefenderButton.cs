e1a95340a3145a04894c58c9331b32c54bc9ec78
Lara <lara.durrant@gmail.com>
2019-05-02 12:54:02 -0700

    Add UI base for defender selection menu

---
+++ Assets/Scripts/DefenderButton.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DefenderButton : MonoBehaviour {
+
+    ColorBlock original_color;
+
+    public void ClickButton()
+    {
+        Debug.Log("defender clicked");
+
+        var buttons = FindObjectsOfType<DefenderButton>();
+
+        foreach(DefenderButton button in buttons)
+        {
+            original_color = button.GetComponent<Button>().colors;
+                //this.gameObject.GetComponent<Button>().colors;
+
+            original_color.normalColor = new Color(.2f, .2f, .2f);
+
+            button.GetComponent<Button>().colors = original_color;
+        }
+        
+        original_color = this.gameObject.GetComponent<Button>().colors;
+
+        original_color.normalColor = new Color(1f, 1f, 1f);
+
+        this.gameObject.GetComponent<Button>().colors = original_color;
+        
+    }
+
+}
